4d83268605f08eadb66154bb959fc824991ee0e8
stypojulvier009-dev <stypojulvier009@gmail.com>
2026-03-31 01:55:04 +0200

    Refactor database connection settings

--- database/db.js
+++ database/db.js
@@ -5,10 +5,7 @@ const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
     ssl: {
         rejectUnauthorized: false
-    },
-    max: 20,
-    idleTimeoutMillis: 30000,
-    connectionTimeoutMillis: 2000,
+    }
 });
 
 pool.on('connect', () => {
