0ac9ff7c01fc555f45d94bdbd7bdb2e3ec080952
stypojulvier009-dev <stypojulvier009@gmail.com>
2026-03-31 14:27:01 +0200

    Update isAdmin.js

--- middleware/isAdmin.js
+++ middleware/isAdmin.js
@@ -1,6 +1,7 @@
+// middleware/isAdmin.js
 module.exports = (req, res, next) => {
-  if (req.user.role !== 'admin') {
-    return res.status(403).json({ error: 'Accès réservé à l’administrateur' });
-  }
-  next();
+    if (req.user.role !== 'admin') {
+        return res.status(403).json({ error: 'Accès réservé à l’administrateur' });
+    }
+    next();
 };
